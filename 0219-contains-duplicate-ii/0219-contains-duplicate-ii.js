57d1a2896279baa39f57f860ae3d11432bd82c24
Navdeep Shah <navshah00001@gmail.com>
2023-10-21 02:57:27 +0530

    Time: 4236 ms (17.62%), Space: 50 MB (94.12%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    const onj={}
+    for(let i=0;i<nums.length;i++)
+    {
+        for(let j=i+1;j<nums.length;j++)
+        {
+             if(nums[i]===nums[j] && Math.abs(i-j)<=k)
+                return true;
+        }
+    }
+    return false;
+};
