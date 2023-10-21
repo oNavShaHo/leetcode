d2fd7204d1115956293901a5e10017d885113605
Navdeep Shah <navshah00001@gmail.com>
2023-10-22 03:41:20 +0530

    Time: 57 ms (30.47%), Space: 42.6 MB (45.93%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -1,14 +1,46 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
 var searchRange = function(nums, target) {
-    
+    const firstIndex = findFirstIndex(nums, target);
+    const lastIndex = findLastIndex(nums, target);
 
-const firstIndex = nums.indexOf(target);
-const lastIndex = nums.lastIndexOf(target);
-    
-return [firstIndex, lastIndex];
-
+    return [firstIndex, lastIndex];
 };
+
+function findFirstIndex(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    let firstIndex = -1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        if (nums[mid] >= target) {
+            if (nums[mid] === target) {
+                firstIndex = mid;
+            }
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return firstIndex;
+}
+
+function findLastIndex(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    let lastIndex = -1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        if (nums[mid] <= target) {
+            if (nums[mid] === target) {
+                lastIndex = mid;
+            }
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return lastIndex;
+}
