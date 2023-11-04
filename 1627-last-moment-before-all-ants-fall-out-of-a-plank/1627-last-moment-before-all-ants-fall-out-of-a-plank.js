71123636cd71a581250f4da2deca67ecddc900cb
Navdeep Shah <navshah00001@gmail.com>
2023-11-05 00:37:46 +0530

    Time: 58 ms (50%), Space: 44.5 MB (83.33%) - LeetHub

---
+++ 1627-last-moment-before-all-ants-fall-out-of-a-plank/1627-last-moment-before-all-ants-fall-out-of-a-plank.js
@@ -0,0 +1,5 @@
+var getLastMoment = function(n, left, right) {
+    const maxLeft = left.length === 0 ? 0 : Math.max(...left);
+    const minRight = right.length === 0 ? n : Math.min(...right);
+    return Math.max(maxLeft, n - minRight);
+};
